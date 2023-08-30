3eb7c53dbbcaf5fe199af0adccafd97cfab675e2
wjfwp <sis6710@gmail.com>
2023-08-30 14:39:49 +0900

    조금했음

---
+++ src/main/java/com/court/proj/user/MypageController.java
@@ -0,0 +1,30 @@
+package com.court.proj.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/userMypage")
+public class MypageController {
+
+	@GetMapping("/")
+	public String mypage() {
+		return "userMypage/mypage";
+	}
+	
+	@GetMapping("/history")
+	public String history() {
+		return "userMypage/history";
+	}
+	
+	@GetMapping("/pause")
+	public String pause() {
+		return "userMypage/pause";
+	}
+	
+	@GetMapping("/status")
+	public String status() {
+		return "userMypage/status";
+	}
+}
