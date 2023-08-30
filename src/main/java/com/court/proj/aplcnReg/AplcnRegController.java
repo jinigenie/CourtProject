550f70c8d2620b0a5f5c5e9714a3651ae41ec8f3
jinigenie <genie@gmail.com>
2023-08-30 17:17:04 +0900

    "layout change test"

--- src/main/java/com/court/proj/aplcnReg/AplcnRegController.java
+++ src/main/java/com/court/proj/aplcnReg/AplcnRegController.java
@@ -5,12 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/aplcn")
+@RequestMapping("/app")
 public class AplcnRegController {
 
     @GetMapping("/info")
     public String getInfo() {
-        return "aplcnRegInfo";
+        return "app/aplcnRegInfo";
     }
+
+    @GetMapping("/career")
+    public String getCareer() {
+        return "app/aplcnRegCareerAndCert";
+    }
+
+    @GetMapping("/test")
+    public String teste() {
+        return "app/test";
+    }
+
 
 }
