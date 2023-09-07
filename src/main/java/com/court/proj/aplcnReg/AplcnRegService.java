1aa3a66119f40a55811598cef30edd29da2eaded
jinigenie <genie@gmail.com>
2023-09-07 16:04:09 +0900

    "table.txt 수정, 기본정보 불러오기 구현_20230907"

--- src/main/java/com/court/proj/aplcnReg/AplcnRegService.java
+++ src/main/java/com/court/proj/aplcnReg/AplcnRegService.java
@@ -1,14 +1,17 @@
 package com.court.proj.aplcnReg;
 
+import com.court.proj.user.UserVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.ArrayList;
 
 public interface AplcnRegService {
 
-    void setCerti(CertiVO vo);
+    public UserVO getInfo(String userId);
 
-    public ArrayList<CertiVO> getCerti(@Param("vo") CertiVO vo);
+
+//    void setCerti(CertiVO vo);
+//    public ArrayList<CertiVO> getCerti(@Param("vo") CertiVO vo);
 
 
 }
