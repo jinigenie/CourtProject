8cca6d68d0cdc54ed8f355613e74d6b6b413d38f
shinkyle222 <shinkyle222@gmail.com>
2023-09-08 18:06:53 +0900

    네번째 신청자 목록, 평가, 상세정보 수정 (쿼리스트링으로 값 넘기고, Mapper부분 수정)

--- src/main/java/com/court/proj/aplcn/AplcnMapper.java
+++ src/main/java/com/court/proj/aplcn/AplcnMapper.java
@@ -5,9 +5,17 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.court.proj.aplcn.util.Criteria;
+
 @Mapper
 public interface AplcnMapper {
 
+	// public int aplcn_evaluate();
+	public ArrayList<ListVO> getList(@Param("user_proper_num") int user_proper_num, @Param("cri") Criteria cri);
+	public int getTotal(@Param("user_proper_num") int user_proper_num, @Param("cri") Criteria cri); // 전체게시글수
 	
+	public ListVO getDetails(@Param("aplcn_dtls_proper_num") int aplcn_dtls_proper_num);
 	
+	public int getEvaluate(ListVO vo);
+
 }
