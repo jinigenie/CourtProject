7c0407108e9ae4b74bc9bfa97e18460d23a686bf
gkekf123 <taeju95@gmail.com>
2023-09-08 12:18:18 +0900

    공지 등록 구현중

--- src/main/java/com/court/proj/announce/AnnounceController.java
+++ src/main/java/com/court/proj/announce/AnnounceController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/announce")
@@ -21,9 +24,7 @@ public class AnnounceController {
 	@GetMapping("announceList")
 	public String announceList(Model model) {
 		
-		String admin = "admin";
-		
-		ArrayList<AnnounceVO> list = announceService.getList(admin);
+		ArrayList<AnnounceVO> list = announceService.getannounceList();
 		
 		model.addAttribute("list", list);
 		
@@ -45,9 +46,50 @@ public class AnnounceController {
 	// 모집공고 등록 페이지
 	@GetMapping("announceRegist")
 	public String announceRegist() {
+		
+		
+		
 		return "announce/announceRegist";
 	}
 	
-	
+	// 모집공고 등록 폼 요청
+	@PostMapping("/announceRegistForm")
+	public String announceRegistForm(
+	    @RequestParam("announce_title") String announceTitle,
+	    @RequestParam("announce_content") String announceContent,
+	    @RequestParam("announce_start_date") String startDate,
+	    @RequestParam("announce_end_date") String endDate,
+	    @RequestParam("trial_fcltt_main_code") String trialfclttMaincode,
+	    @RequestParam("trial_fcltt_clasifi_code") String trialfclttClasificode,
+	    @RequestParam("trial_fcltt_sbcls_code") String trialfclttSbclscode,
+	    
+	    RedirectAttributes ra) {
+
+	    AnnounceVO vo = AnnounceVO.builder()
+	        .announce_title(announceTitle)
+	        .announce_content(announceTitle)
+	        .announce_content(startDate)
+	        .announce_end_date(endDate)
+	        .trial_fcltt_main_code(trialfclttMaincode)
+	        .trial_fcltt_clasifi_code(trialfclttClasificode)
+	        .trial_fcltt_sbcls_code(trialfclttSbclscode)
+	        .build();
+
+	    try {
+	        int result = announceService.announceRegist(vo);
+
+	        if (result > 0) {
+	            ra.addFlashAttribute("공고등록 성공", "모집공고가 성공적으로 등록되었습니다.");
+	        } else {
+	            ra.addFlashAttribute("공고등록 오류", "모집공고 등록 중 오류가 발생하였습니다.");
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        ra.addFlashAttribute("공고등록 오류", "모집공고 등록 중 오류가 발생하였습니다.");
+	    }
+
+	    return "redirect:/announce/announceList";
+	}
+
 	
 }
