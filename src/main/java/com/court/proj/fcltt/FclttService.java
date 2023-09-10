6c2163919907037e06ed22f0f8330df017fcea4c
hyemi <hyemi@175.193.121.252>
2023-09-10 22:24:01 +0900

    .

--- src/main/java/com/court/proj/fcltt/FclttService.java
+++ src/main/java/com/court/proj/fcltt/FclttService.java
@@ -8,7 +8,7 @@ public interface FclttService {
 	// 조력자 등재
 	public int fclttRegist(FclttVO vo);
 	
-	// 조력자명단조회
+	// 조력자명단조회ㄹ
 	public ArrayList<FclttVO> getList(FclttCriteria cri);  //조회  페이징하기 (criteria 객체생성)
 	public int getTotal(FclttCriteria cri);
 	
