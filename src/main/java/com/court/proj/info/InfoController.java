8fe786a20d8ba833355accf9346aa886abec48eb
hyemi <hyemi@memi>
2023-09-19 16:41:17 +0900

    info test

---
+++ src/main/java/com/court/proj/info/InfoController.java
@@ -0,0 +1,19 @@
+package com.court.proj.info;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/info")
+public class InfoController {
+
+	@GetMapping("/info")
+	public String info() {
+		
+		return "/info/info";
+	}
+	
+	
+	
+}
