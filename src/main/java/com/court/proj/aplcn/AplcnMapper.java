def8a63f2db3facb9237aa24d4e8900ee637ca29
shinkyle222 <shinkyle222@gmail.com>
2023-09-21 10:18:29 +0900

    아홉번째 수정 신청자 평가, 목록, 상세정보

--- src/main/java/com/court/proj/aplcn/AplcnMapper.java
+++ src/main/java/com/court/proj/aplcn/AplcnMapper.java
@@ -4,24 +4,31 @@ import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-
 import com.court.proj.aplcn.util.Criteria;
 
 @Mapper
 public interface AplcnMapper {
 
-	//신청자 리스트
-	public ArrayList<ListVO> getList(@Param("user_proper_num") int user_proper_num, @Param("cri") Criteria cri);
-	public int getTotal(@Param("user_proper_num") int user_proper_num, @Param("cri") Criteria cri); // 전체게시글수
-	
-	//신청자 상세정보
+	// 신청자 리스트
+	public ArrayList<ListVO> getList(@Param("cri") Criteria cri);
+
+	public int getTotal(@Param("cri") Criteria cri); // 전체게시글수
+
+	// 신청자 상태변환 (서류반려)
+	public int aplcnReject(ListVO vo);
+
+	// 신청자 상태변환 (평가완료)
+	public int aplcnCompleted(ListVO vo);
+
+	// 신청자 상세정보
 	public ArrayList<ListVO> getDetails(@Param("aplcn_dtls_proper_num") int aplcn_dtls_proper_num);
+
 	public ListVO getDetail(@Param("aplcn_dtls_proper_num") int aplcn_dtls_proper_num);
-	
-	//신청자 평가 (수정중)
+
+	// 신청자 평가
 	public int aplcnEvaluate(ListVO vo);
-	
-	//파일 다운로드
+
+	// 파일 다운로드
 	public ListVO aplcnFiles(@Param("aplcn_dtls_proper_num") int aplcn_dtls_proper_num);
 
 }
--- src/main/java/com/court/proj/aplcn/AplcnService.java
+++ src/main/java/com/court/proj/aplcn/AplcnService.java
@@ -7,18 +7,28 @@ import org.apache.ibatis.annotations.Param;
 import com.court.proj.aplcn.util.Criteria;
 
 public interface AplcnService {
+
+	// 신청자 리스트
+	public ArrayList<ListVO> getList(Criteria cri);
+
+	// 전체 게시글 수
+	public int getTotal(Criteria cri); 
+
+	// 신청자 상세정보 (복수값)
+	public ArrayList<ListVO> getDetails(int aplcn_dtls_proper_num); 
+
+	// 신청자 상세정보 (단수값)
+	public ListVO getDetail(int aplcn_dtls_proper_num);
+
+	// 신청자 상태변환 (서류반려)
+	public int aplcnReject(ListVO vo);
 	
-	//신청자 리스트
-	public ArrayList<ListVO> getList(int user_proper_num, Criteria cri);
-	public int getTotal(int user_proper_num, Criteria cri); //전체 게시글 수
-	
-	//신청자 상세정보
-	public ArrayList<ListVO> getDetails(int aplcn_dtls_proper_num); //상세
-	public ListVO getDetail(int aplcn_dtls_proper_num); 
-	
-	//신청자 평가 (수정중)
+	// 신청자 상태변환 (평가완료)
+	public int aplcnCompleted(ListVO vo);
+
+	// 신청자 평가
 	public int aplcnEvaluate(ListVO vo);
-	
-	//파일 다운로드
+
+	// 파일 다운로드
 	public ListVO aplcnFiles(int aplcn_dtls_proper_num);
 }
